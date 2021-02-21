fbdfbd9390034c51e0762da011df664d8bdff530
Vadim Alekseev <olorinoren@gmail.com>
2021-02-21 16:28:17 +0300

    add feature:: automatic determination of the start date and the end of the task depending on the period

---
+++ ViewModels/TaskEditModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgileResultsMVC.ViewModels
+{
+    public class TaskEditModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
