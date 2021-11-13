575994a4261ef215894f7cf7bcc392291ddbbd86
CountNeuroman <olorinoren@gmail.com>
2021-11-13 14:56:55 +0300

    Update to NET 6

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace AgileResultsMVC.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using AgileResultsMVC.Data;
+using AgileResultsMVC.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace AgileResultsMVC
 {
@@ -13,14 +13,39 @@ namespace AgileResultsMVC
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AgileResultsMVCContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("AgileResultsMVCContext")));
+
+            builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<AgileResultsMVCContext>();
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run();
+        }
     }
 }
